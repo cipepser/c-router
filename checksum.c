7303959abd6615d12869e60edcb987278c5b278f
cipepser <respepic@gmail.com>
2018-05-19 15:44:43 +0900

    implement checksum

--- checksum.c
+++ checksum.c
@@ -14,3 +14,48 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+struct pseudo_ip {
+  struct in_addr ip_src;
+  struct in_addr ip_dst;
+  unsigned char dummy;
+  unsigned char ip_p;
+  unsigned short ip_len;
+};
+
+struct pseudo_ip6_hdr {
+  struct in6_addr src;
+  struct in6_addr dst;
+  unsigned long plen;
+  unsigned short dmy1;
+  unsigned char dmy2;
+  unsigned char nxt;
+};
+
+u_int16_t checksum(u_char *data, int len) {
+  register u_int32_t sum;
+  register u_int16_t *ptr;
+  register int c;
+
+  sum = 0;
+  ptr = (u_int16_t *)data;
+
+  for (c = len; c > 1; c -= 2) {
+    sum += (*ptr);
+    if (sum & 0x80000000) {
+      sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+    ptr++;
+  }
+
+  if (c == 1) {
+    u_int16_t val;
+    val = 0;
+    memcpy(&val, ptr, sizeof(u_int8_t));
+    sum += val;
+  }
+
+  whike(sum >> 16) { sum = (sum & 0xFFFF) + (sum >> 16); }
+
+  return (~sum);
+}
