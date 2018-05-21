3f85bbd384184f396838b95c71560e421d6e0fb3
cipepser <respepic@gmail.com>
2018-05-21 21:36:09 +0900

    implement AnalyzePacket

--- router/main.c
+++ router/main.c
@@ -102,3 +102,133 @@ int SendIcmpTimeExceeded(int deviceNo, struct ether_header *eh,
 
   return (0);
 }
+
+int AnalyzePacket(int deviceNo, u_char *data, int size) {
+  u_char *ptr;
+  int lest;
+  struct ether_header *eh;
+  char buf[80];
+  int tno;
+  u_char hwaddr[6];
+
+  ptr = data;
+  lest = size;
+
+  if (lest < sizeof(struct ether_header)) {
+    DebugPrintf("[%d]:lest(%d) < sizeof(struct ether_header)\n", deviceNo,
+                lest);
+    return (-1);
+  }
+
+  eh = (struct ether_header *)ptr;
+  ptr += sizeof(struct ether_header);
+  lest -= sizeof(struct ether_header);
+
+  if (memcmp(&eh->ether_dhost, Device[deviceNo].hwaddr, 6) != 0) {
+    DebugPrintf("[%d]:dhost not match %s\n", deviceNo,
+                my_ether_ntoa_r((u_char *)&eh->ether_dhost, buf, sizeof(buf)));
+    return (-1);
+  }
+
+  if (ntohs(eh->ether_type) == ETHERTYPE_ARP) {
+    struct ether_arp *arp;
+
+    if (lest < sizeof(struct ether_arp)) {
+      DebugPrintf("[%d]:lest(%d) < sizeof(struct ether_arp)\n", deviceNo, lest);
+      return (-1);
+    }
+
+    arp = (struct ether_arp *)ptr;
+    ptr += sizeof(struct ether_arp);
+    lest -= sizeof(struct ether_arp);
+
+    if (arp->arp_op == htons(ARPOP_REQUEST)) {
+      DebugPrintf("[%d]recv: ARP REQUEST: %dbytes\n", deviceNo, size);
+      Ip2Mac(deviceNo, *(in_addr_t *)arp->arp_spa, arp->arp_spa);
+    }
+    if (arp->arp_op == htons(ARPOP_REPLY)) {
+      DebugPrintf("[%d]recv: ARP REPLY: %dbytes\n", deviceNo, size);
+      Ip2Mac(deviceNo, *(in_addr_t *)arp->arp_spa, arp->arp_spa);
+    }
+  } else if (ntohs(eh->ether_type) == ETHERTYPE_IP) {
+    struct iphdr *iphdr;
+    u_char option[1500];
+    int optionLen;
+
+    if (lest < sizeof(struct iphdr)) {
+      DebugPrintf("[%d]:lest(%d) < sizeof(struct iphdr)\n", deviceNo, lest);
+      return (-1);
+    }
+    iphdr = (struct iphdr *)ptr;
+    ptr += sizeof(struct iphdr);
+    size -= sizeof(struct iphdr);
+
+    optionLen = iphdr->ihl * 4 - sizeof(struct iphdr);
+    if (optionLen > 0) {
+      if (optionLen >= 1500) {
+        DebugPrintf("[%d]:IP option Len(%d); too big\n", deviceNo, optionLen);
+        return (-1);
+      }
+
+      memcpy(option, ptr, optionLen);
+      ptr += optionLen;
+      size -= optionLen;
+    }
+
+    if (checkIPchecksum(iphdr, option, optionLen) == 0) {
+      DebugPrintf("[%d]:bad ip checksum", deviceNo);
+      return (-1);
+    }
+    
+    if (iphdr->ttl - 1 == 0) {
+      DebugPrintf("[%d]: iphdr->ttl == 0 error\n", deviceNo);
+      SendIcmpTimeExceeded(deviceNo, eh, iphdr, data, size);
+      return (-1);
+    }
+    
+    tno = (!deviceNo);
+    
+    if ((iphdr->addr & Device[tno].netmask.s_addr) == Device[tno].netmask.s_addr) {
+      IP2MAc *ip2mac;
+      
+      DebugPrintf("[%d]:%s to TargetSegment\n", deviceNo, in_addr_t2str(iphdr->daddr, buf, sizeof(buf)));
+      
+      if (iphdr->daddr == Device[tno].addr.s_addr) {
+        DebugPrintf("[%d]:recv:myaddr\n", deviceNo);
+        return (1);
+      }
+      
+      ip2mac = Ip2Mac(tno, iphdr->daddr, NULL);
+      if (ip2mac->flag == FLAG_NG || ip2mac->sd.dno != 0) {
+        DebugPrintf("[%d]:Ip2Mac:error or sending\n", deviceNo);
+        AppendSendData(ip2mac, 1, iphdr->daddr, data, size);
+        return (-1);
+      } else {
+        memcpy(hwaddr, ip2mac->hwaddr, 6);
+      }
+    } else {
+      IP2MAc *ip2mac;
+      
+      DebugPrintf("[%d]:%s to NextRouter\n", deviceNo, in_addr_t2str(iphdr->daddrm buf, sizeof(buf)));
+      
+      ip2mac = Ip2Mac(tno, NextRouter.s_addr, NULL);
+      if (ip2mac->flag == FLAG_NG || ip2mac->ad.dno != 0) {
+        DebugPrintf("[%d]:Ip2Mac:error or sending\n", deviceNo);
+        AppendSendData(ip2mac, 1, NextRouter.s_addr, data, size);
+        return (-1);
+      } else {
+        memcpy(hwaddr, ip2mac->hwaddr, 6);
+      }
+    }
+    memcpy(eh->ether_dhost, hwaddr, 6);
+    memcpy(eh->ether_shost, Device[tno].hwaddr, 6);
+    
+    iphdr->ttl--;
+    iphdr->check = 0;
+    iphdr->check = checksum2((u_char *)iphdr, sizeof(struct iphdr), option, optionLen);
+    
+    write(Device[tno].soc, data, size);
+  }
+  
+  return (0);
+}
