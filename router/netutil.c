762c52ea9b60ee92377e98930b0d679e73d93b09
cipepser <respepic@gmail.com>
2018-05-23 23:46:57 +0900

    implement PrintEtherHader

--- router/netutil.c
+++ router/netutil.c
@@ -140,6 +140,33 @@ char *in_addr_t2str(in_addr_t addr, char *buf, socklen_t size) {
   return (buf);
 }
 
+int PrintEtherHeader(struct ether_header *eh, FILE *fp) {
+  char buf[80];
+
+  fprintf(fp, "ether_header-----------------------\n");
+  fprintf(fp, "ether_dhost=%s\n",
+          my_ether_ntoa_r(eh->ether_dhost, buf, sizeof(buf)));
+  fprintf(fp, "ether_shost=%s\n",
+          my_ether_ntoa_r(eh->ether_shost, buf, sizeof(buf)));
+  fprintf(fp, "ether_type=%02X\n", ntohs(eh->ether_type));
+
+  switch (ntohs(eh->ether_type)) {
+  case ETH_P_IP:
+    fprintf(fp, "(IP)\n");
+    break;
+  case ETH_P_IPV6:
+    fprintf(fp, "(IPv6)\n");
+    break;
+  case ETH_P_ARP:
+    fprintf(fp, "(ARP)\n");
+    break;
+  default:
+    fprintf(fp, "(unknown)\n");
+    break;
+  }
+
+  return (0);
+}
 
 
 
