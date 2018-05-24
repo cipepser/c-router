e3b2fb2a148b7950c12a82aedc1f32ec6ba0a968
cipepser <respepic@gmail.com>
2018-05-24 23:04:03 +0900

    implement SendArpRequestB

--- router/netutil.c
+++ router/netutil.c
@@ -133,10 +133,10 @@ char *my_inet_ntoa_r(struct in_addr *addr, char *buf, socklen_t size) {
 
 char *in_addr_t2str(in_addr_t addr, char *buf, socklen_t size) {
   struct in_addr a;
-  
+
   a.s_addr = addr;
   inet_ntop(PF_INET, &a, buf, size);
-  
+
   return (buf);
 }
 
@@ -252,7 +252,7 @@ u_int16_t checksum2(u_char *data1, int len1, u_char *data2, int len2) {
 int checkIPchecksum(struct iphdr *iphdr, u_char *option, int optionLen) {
   struct iphdr iptmp;
   unsigned short sum;
-  
+
   memcpy(&iptmp, iphdr, sizeof(struct iphdr));
 
   if (optionLen == 0) {
@@ -272,13 +272,71 @@ int checkIPchecksum(struct iphdr *iphdr, u_char *option, int optionLen) {
   }
 }
 
+typedef struct {
+  struct ether_header eh;
+  struct ether_arp arp;
+} PACKET_ARP;
 
+int SendArpRequestB(int soc, in_addr_t target_ip, u_char target_mac[6],
+                    in_addr_t my_ip, u_char my_mac[6]) {
+  PACKET_ARP arp;
+  int total;
+  u_char *p;
+  u_char buf[sizeof(struct ether_header) + sizeof(struct ether_arp)];
+  union {
+    unsigned long l;
+    u_char c[4];
+  } lc;
+  int i;
 
+  arp.arp.arp_hrd = htons(ARPHRD_ETHER);
+  arp.arp.arp_pro = htons(ETHERTYPE_IP);
+  arp.arp.arp_hln = 6;
+  arp.arp.arp_pln = 4;
+  arp.arp.arp_op = htons(ARPOP_REQUEST);
 
+  for (i = 0; i < 6; i++) {
+    arp.arp.arp_sha[i] = my_mac[i];
+  }
+  for (i = 0; i < 6; i++) {
+    arp.arp.arp_tha[i] = 0;
+  }
 
+  lc.l = my_ip;
+  for (i = 0; i < 4; i++) {
+    arp.arp.arp_spa[i] = lc.c[i];
+  }
 
+  lc.l = target_ip;
+  for (i = 0; i < 4; i++) {
+    arp.arp.arp_tpa[i] = lc.c[i];
+  }
 
+  arp.eh.ether_dhost[0] = target_mac[0];
+  arp.eh.ether_dhost[1] = target_mac[1];
+  arp.eh.ether_dhost[2] = target_mac[2];
+  arp.eh.ether_dhost[3] = target_mac[3];
+  arp.eh.ether_dhost[4] = target_mac[4];
+  arp.eh.ether_dhost[5] = target_mac[5];
 
+  arp.eh.ether_shost[0] = my_mac[0];
+  arp.eh.ether_shost[1] = my_mac[1];
+  arp.eh.ether_shost[2] = my_mac[2];
+  arp.eh.ether_shost[3] = my_mac[3];
+  arp.eh.ether_shost[4] = my_mac[4];
+  arp.eh.ether_shost[5] = my_mac[5];
 
+  arp.eh.ether_type = htons(ETHERTYPE_ARP);
 
+  memset(buf, 0, sizeof(buf));
+  p = buf;
+  memcpy(p, &arp.eh, sizeof(struct ether_header));
+  p += sizeof(struct ether_header);
+  memcpy(p, &arp.arp, sizeof(struct ether_arp));
+  p += sizeof(struct ether_arp);
+  total = p - buf;
 
+  write(soc, buf, total);
+
+  return (0);
+}
